e5c103d533d0cfd72ce8b8a9e50750ce55516102
gaofeng222 <596642721@qq.com>
2025-05-14 15:06:44 +0800

    fix:修复付款链接的环境区分

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -60,7 +60,7 @@ onMounted(() => { getPayInfo() })
 // 携带订单id以及回调地址跳转到支付地址（get）
 // 支付地址
 const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
-const backURL = 'http://127.0.0.1:5173/#/paycallback'
+const backURL = `${import.meta.env.VITE_REDIRECT_URL}/#/paycallback`
 const redirectUrl = encodeURIComponent(backURL)
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
